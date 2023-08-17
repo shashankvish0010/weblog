3afc2e27fd7e07ec9cf09c161248ff620553af24
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-08-17 23:03:28 +0530

    database added

---
+++ server/database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE weblog;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
+    email VARCHAR(30),
+    user_password VARCHAR
+);
