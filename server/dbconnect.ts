097bec12916b597736dfa74a3363c1561fa03244
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-03 22:47:19 +0530

    Added env config

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -1,5 +1,6 @@
+import dotenv from 'dotenv'
 import { Pool } from 'pg'
-
+dotenv.config()
 const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
 :${process.env.DB_PORT}/${process.env.DB_Database}`
 
--- server/dist/dbconnect.js
+++ server/dist/dbconnect.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const pg_1 = require("pg");
+dotenv_1.default.config();
 const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
 :${process.env.DB_PORT}/${process.env.DB_Database}`;
 const proConfig = process.env.DB_ProdUrl;
