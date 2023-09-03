34a0b3b02beac57b6247e9c882f86f0282e3d8a1
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-03 22:12:53 +0530

    Applied Changes

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -3,7 +3,9 @@ import { Pool } from 'pg'
 const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
 :${process.env.DB_PORT}/${process.env.DB_Database}`
 
+const proConfig = process.env.DB_ProdUrl
+
 const pool = new Pool({
-    connectionString: DatabaseUrl
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : DatabaseUrl
 });
 export default pool
--- server/dist/dbconnect.js
+++ server/dist/dbconnect.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
 :${process.env.DB_PORT}/${process.env.DB_Database}`;
+const proConfig = process.env.DB_ProdUrl;
 const pool = new pg_1.Pool({
-    connectionString: DatabaseUrl
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : DatabaseUrl
 });
 exports.default = pool;
