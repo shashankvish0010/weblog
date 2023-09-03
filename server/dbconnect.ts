c87221fe2dfcce079290767c00eb691dff10b1b0
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-03 21:55:18 +0530

    Add Connection String

--- server/dbconnect.ts
+++ server/dbconnect.ts
@@ -1,9 +1,9 @@
-import {Pool} from 'pg'
+import { Pool } from 'pg'
 
-const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_HOST}
-:${process.env.DB_PORT}:${process.env.DB_Database}`
+const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
+:${process.env.DB_PORT}/${process.env.DB_Database}`
 
- const pool = new Pool({
+const pool = new Pool({
     connectionString: DatabaseUrl
- });
+});
 export default pool
--- server/dist/dbconnect.js
+++ server/dist/dbconnect.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_HOST}
-:${process.env.DB_PORT}:${process.env.DB_Database}`;
+const DatabaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}
+:${process.env.DB_PORT}/${process.env.DB_Database}`;
 const pool = new pg_1.Pool({
     connectionString: DatabaseUrl
 });
