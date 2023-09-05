2e3727225031cf554f9ee573fd8839c49f904fa2
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-05 16:21:13 +0530

    add

--- client/weblog/src/main.tsx
+++ client/weblog/src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 import {RegisterContextProvider} from './Context/UserReg.tsx'
