b23cec3aa6baf6fdb25c80f0ceb53aaa4b11344e
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-07 01:49:45 +0530

    add

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,9 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 app.use(bodyParser.json({ limit: '10mb' }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-app.use(cors({
-    origin: "https://weblog-lake.vercel.app"
-}))
+app.use(cors())
 app.use(require('./routers/routes'))
 
 app.listen(process.env.PORT, () => console.log("Server running"))
