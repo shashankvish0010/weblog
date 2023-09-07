9e5e2e00da6fa9b2a53fa1fb3050b88c8dce7427
Shashank Vishwakarma <shashank000s4@gmail.com>
2023-09-07 13:13:15 +0530

    Added

--- client/weblog/vite.config.ts
+++ client/weblog/vite.config.ts
@@ -4,28 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/search/post': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/send/updates': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/user/register': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/user/login': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/admin/register': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/admin/login': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/user/logout': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/admin/logout': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/add/subscriber': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/edit/profile': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/edit/blogpost': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/unsubscribe': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/publish/blogpost': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/api/posts': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/view/post': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/fetch/user/posts': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/delete/post': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/flag/post': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/verify/account/': { target: 'https://weblog-backend-247o.onrender.com' },
-      '/verify/account/re': { target: 'https://weblog-backend-247o.onrender.com' },
-    }
+    // proxy: {
+    //   '/api': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/search/post': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/send/updates': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/user/register': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/user/login': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/admin/register': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/admin/login': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/user/logout': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/admin/logout': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/add/subscriber': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/edit/profile': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/edit/blogpost': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/unsubscribe': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/publish/blogpost': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/api/posts': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/view/post': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/fetch/user/posts': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/delete/post': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/flag/post': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/verify/account/': { target: 'https://weblog-backend-247o.onrender.com' },
+    //   '/verify/account/re': { target: 'https://weblog-backend-247o.onrender.com' },
+    // }
   }
 })
