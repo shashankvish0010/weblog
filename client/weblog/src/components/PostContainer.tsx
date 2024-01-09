08657e7604c6708fac5c8e4c39844082f5d021d7
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-01-10 02:49:02 +0530

    Added: changes to post container

--- client/weblog/src/components/PostContainer.tsx
+++ client/weblog/src/components/PostContainer.tsx
@@ -45,17 +45,17 @@ const PostContainer: React.FC<blogpostbody> = ({ id, title, image, meta, firstna
     }
     // const { id, title, image, meta, firstname, lastname } = props;
     return (
-        <div className='h-max md:w-[40vw] w-[80vw] shadow-2xl rounded flex flex-col p-3 gap-4 object-fill items-center'>
+        <div className='bg-gradient-to-r from-indigo-600 via-pink-600 to-orange-600 text-white h-max md:w-[40vw] w-[80vw] shadow-2xl rounded flex flex-col p-3 gap-4 object-fill items-center'>
             <div className='h-[200px]'>
-                <img src={image} className='h-[200px] w-[400px] object-cover' />
+                <img src={image} className='h-[200px] w-[400px] shadow-xl object-cover' />
             </div>
-            <span className='rounded-md h-[0.15rem] bg-indigo-600 w-[100%]'></span>
+            <span className='rounded-md h-[0.15rem] bg-white w-[100%]'></span>
 
-            <h2 className=' font-medium text-xl'>{title}</h2>
-            <p>{meta.length > 150 ? meta.slice(0, 150) : meta}...</p>
+            <h2 className='title text-xl shadow-sm'>{title}</h2>
+            <p className='italic shadow-sm font-medium text-slate-100 text-base'>{meta.length > 150 ? meta.slice(0, 150) : meta}...</p>
             <div className='md:h-[3vh] h-max gap-2 w-[100%] flex md:flex-row flex-col items-center justify-evenly'>
-                <p className='text-base font-medium'>Posted: {firstname} <span className='text-indigo-600'>{lastname}</span></p>
-                <button onClick={() => LoadPost(id)} className='bg-indigo-600 font-medium shadow-md rounded-sm px-1 text-base text-white'>
+                <p className='meta_desc text-base shadow-sm font-medium'>Posted: {firstname} {lastname}</p>
+                <button onClick={() => LoadPost(id)} className='bg-indigo-600 font-medium shadow-md rounded-sm p-1 text-base'>
                     Read More
                 </button>
             </div>
