2a2cecec7e46e20e33549012c066ab2180562e55
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-04-25 01:14:48 +0530

    Debug

--- client/weblog/src/pages/Cpanel.tsx
+++ client/weblog/src/pages/Cpanel.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import ProfilePost from '../components/ProfilePost'
 import { postContext } from '../Context/PostData'
 import { Icon } from '@iconify/react'
@@ -8,8 +8,10 @@ import { Link } from 'react-router-dom'
 const Cpanel: React.FC = () => {
     const postDetalis = useContext(postContext)
     const adminInfo = useContext(AdminContext)
+    useEffect(()=> console.log(adminInfo)
+    , [adminInfo])
     return (
-        (adminInfo?.status.success === true) ?
+        (adminInfo?.status.success == true) ?
             <div className='h-max w-[100vw] flex flex-col justify-center gap-5 p-3'>
                 <div className='h-max w-screen flex flex-row justify-around'>
                     <Link to='/sendmail'>
